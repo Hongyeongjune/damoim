81e734408854d986cb50f7152c57590c6b80db0e
hongyeongjune <hongyeongjune@naver.com>
2020-07-21 16:27:36 +0900

    동네 인증 화면 구현 중

---
+++ app/src/main/java/com/yeongjae/damoim/view/addressverification/AddressVerificationActivity.java
@@ -0,0 +1,16 @@
+package com.yeongjae.damoim.view.addressverification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yeongjae.damoim.R;
+
+public class AddressVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_verification);
+    }
+}
